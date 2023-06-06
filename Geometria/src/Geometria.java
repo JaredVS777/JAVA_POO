6e6090dcd5f205171c27ffb68f2ea8898e5dd184
Jared Valenzuela <jared.valenzuela@epn.edu.ec>
2023-06-06 07:31:55 -0500

    Agreagar constructores, getters and seterrs

--- Geometria/src/Geometria.java
+++ Geometria/src/Geometria.java
@@ -1,3 +1,20 @@
 public class Geometria {
-    
+    int nLados;//numero lados
+
+    public Geometria(int nLados) {//constructor de NLados
+        this.nLados = nLados;
+    }
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    areaCuadrado(){
+
+    }
+
 }
